67270d1b60b26062aa34ca3b7997a593b948b705
nts <ngotungson0602@gmail.com>
2015-06-20 22:15:03 +0700

    Lambda, Block, Proc

--- tryruby_3.rb
+++ tryruby_3.rb
@@ -1,53 +1,47 @@
-	#Lamda and Block
+# lambda is just a function, object like everything else
+l = lambda {"Do or not do"}
+puts l.call()
 
-#Lamda is just a object like everything else in Ruby
-l = lambda { "Do or do not" }
-puts l.call
-
-l = lambda do |string| 
+l = lambda do |string|
   if string == "try"
-    return "There's no such thing" 
-  else
-    return "Do or do not."
-  end
-end
-puts l.call("try") 
+    return "nice"
+  end 
+end 
 
-#Block is  a piece of code that can't be stored in variable and isn't object
-def demonstrate_block(number)
-  yield(number)
-end
-
-puts demonstrate_block(1) { |number| number + 1 }
+puts l.call("try")
 
 
-empty_block = lambda { }
-puts empty_block.object_id
-puts empty_block.class # Proc class(where block come out) is subclass of Object
+#block is a code that you store in variable like other objects and run on demand
+# Proc is a class of lambda and block
+empty_block = lambda {}
+puts empty_block. object_id
+puts empty_block.class
 puts empty_block.class.superclass
 
-# A method is simply a block bound to an object, with access to the object's state
-class Calculator
-  def add(a, b)
-    return a + b
-  end
-end
+def demonstrate_block(number)
+  yield(number)
+end 
 
-addition_method = Calculator.new.method("add")
-addition =  addition_method.to_proc
+puts demonstrate_block(1) { |number| number = number + 1}
 
-puts addition.call(5, 6)
-
-#yield (just a keyword, not a method,use to pass one block to a method)
 def calculation(a, b, operation)
   operation.call(a, b)
 end
 
-puts calculation(5, 6, lambda { |a, b| a + b }) # addition
-puts calculation(5, 6, lambda { |a, b| a - b }) # subtraction
+puts calculation(5, 6, lambda{|a, b| a+b})
+puts calculation(5, 6, lambda{|a, b| a-b})
 
-def foo
- yield
+
+def calculation(a, b) 
+  yield(a, b)
+end 
+
+puts calculation(5, 6) {|a, b| a+b}
+puts calculation(5, 6) {|a, b| a-b}
+
+def calculation(a, b, &block)
+  block.call(a, b)
 end
-foo { puts "sometimes shortcuts do get you there faster"  }
 
+puts calculation(5, 6) {|a, b| a + b}
+puts calculation(5, 6) {|a, b| a - b}
